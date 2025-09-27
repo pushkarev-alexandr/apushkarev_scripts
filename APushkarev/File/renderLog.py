e8ee50744ba6453291615b703211e7050a62de8d
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2025-09-27 21:18:36 +0300

    Refactor: streamline file path handling and improve code readability. Now works correctly for Read nodes with localization enabled. Moved menu creation and callback registration out of the file. Add json file to .gitignore.

--- APushkarev/File/renderLog.py
+++ APushkarev/File/renderLog.py
@@ -1,86 +1,65 @@
 # Saves a pair of keys (file path) and (path to the script that rendered it) to a database
 # Limitation: if the file is moved, the link (file path) -> (script path) is lost
 
-# v1.0.0
+# v1.1.0
 # created by: Pushkarev Aleksandr
 
+# changelog:
+# v1.0.0 - Initial release
+# v1.1.0 - Refactor: streamline file path handling and improve code readability. Now works correctly for Read nodes with localization enabled
+
 # TODO
-# Needs review
-# bake relative paths
+# - use sqlite database instead of json
 
 import nuke, os, json, re
 
 json_path = os.path.dirname(__file__).replace("\\", "/") + "/render_log.json"
 
 def unifyFrameVar(s):
+    """Unify frame variable representation."""
     return re.sub(r"%\d*d", "%d", re.sub(r"#+", "%d", s))
+
+def get_file_path_from_knob(kn):
+    filename = unifyFrameVar(os.path.basename(kn.value()))
+    dirname = os.path.dirname(kn.getEvaluatedValue())
+    return f"{dirname}/{filename}"
 
 def renderLog():
     node = nuke.thisNode()
     nkPath = nuke.root().name()
-    if node.Class()=="Write" and nkPath!="Root":
-        file = node.knob("file").value()
-        file = unifyFrameVar(file)
-        if file and nkPath:
+    if node.Class() == "Write" and nkPath != "Root":
+        path = get_file_path_from_knob(node["file"])
+        if path and nkPath:
             if not os.path.isfile(json_path):
-                f = open(json_path, "w")
-                f.write("{}")
-                f.close()
-            f = open(json_path, "r")
-            json_data = json.load(f)
-            f.close()
-            json_data[file] = nkPath
-            f = open(json_path, "w")
-            json.dump(json_data, f, indent=1)
-            f.close()
+                with open(json_path, "w") as f:
+                    f.write("{}")
+            with open(json_path, "r") as f:
+                json_data = json.load(f)
+            json_data[path] = nkPath
+            with open(json_path, "w") as f:
+                json.dump(json_data, f, indent=1)
 
 def getRelatedScriptPath():
     if not os.path.isfile(json_path):
-        nuke.message("Can't find " + json_path)
+        nuke.message(f"Can't find {json_path}")
         return
+    
     node = nuke.selectedNode()
     kn = node.knob("file")
-    if kn:
-        file = unifyFrameVar(kn.value())
-        f = open(json_path, "r")
+    if not kn:
+        nuke.message("Select Read or Write node")
+        return
+    
+    local_kn = node.knob("localizationPolicy")
+    if local_kn:
+        # Temporarily disable localization to get the correct file path (getEvaluatedValue returns original path only when localization is off)
+        local_value = local_kn.value()
+        local_kn.setValue("off")
+        path = get_file_path_from_knob(kn)
+        local_kn.setValue(local_value)
+    else:
+        path = get_file_path_from_knob(kn)
+    with open(json_path, "r") as f:
         json_data = json.load(f)
-        f.close()
-        nkPath = json_data.get(file)
-        if nkPath:
-            nuke.message(nkPath)
-        else:
-            nuke.message("No data for this file")
-
-nuke.addAfterRender(renderLog)  # Executes in menu.py only for GUI mode to prevent render farm from writing unnecessary data
-nuke.menu("Nuke").addCommand("APushkarev/File/Script Path from Read", "renderLog.getRelatedScriptPath()")
-
-# Script for afanasy render node:
-
-"""
-import os, json, re
-
-json_path = "C:/Users/Fincher/.nuke/python/render_log.json"
-
-def unifyFrameVar(s):
-    return re.sub(r"%\d*d", "%d", re.sub(r"#+", "%d", s))
-
-for node in nuke.thisNode().dependencies():
-    if node.Class()=="Write":
-        nkPath = nuke.root().name()
-        if nkPath!="Root":
-            file = unifyFrameVar(node.knob("file").value())
-            if file and nkPath:
-                if not os.path.isfile(json_path):
-                    f = open(json_path, "w")
-                    f.write("{}")
-                    f.close()
-                f = open(json_path, "r")
-                json_data = json.load(f)
-                f.close()
-                json_data[file] = nkPath
-                f = open(json_path, "w")
-                json.dump(json_data, f, indent=1)
-                f.close()
-
-cgru.render(nuke.thisNode())
-"""
+    nkPath = json_data.get(path)
+    nuke.message(nkPath if nkPath else "No data for this file")
