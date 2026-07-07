2228f204e13deca19bcd2668759588b3319d8825
Pushkarev Aleksandr <pushkarev_alexandr@mail.ru>
2026-07-07 16:54:47 +0300

    gif_render: added support for aces1.3 display mode and fallback to ffmpeg.exe

--- Render/gif_render.py
+++ Render/gif_render.py
@@ -1,12 +1,34 @@
 # Renders a GIF from the selected node to a 'gif' folder next to the script using ffmpeg
 
-# v1.0.0
+# v1.0.1
 # created by: Pushkarev Aleksandr
 
-import nuke
-import subprocess, os, re
+# changelog:
+# v1.0.0 initial version
+# v1.0.1 added support for aces1.3 display mode and fallback to ffmpeg.exe
 
-FFMPEG = "Z:\\Programs\\Utilities\\ffmpeg\\ffmpeg.exe"
+import nuke
+import subprocess, os, re, shutil
+
+FFMPEG_FALLBACK = "Z:\\Programs\\Utilities\\ffmpeg\\ffmpeg.exe"
+FFMPEG = shutil.which("ffmpeg") or FFMPEG_FALLBACK
+
+def setColorspace(node, colorspaces: list[str]) -> None:
+    default_space, aces_space, display_space = colorspaces
+
+    config = nuke.root()["OCIO_config"].value()
+    display = config.startswith("fn-nuke_")
+
+    node["raw"].setValue(False)
+    if node.knob("transformType"):
+        node["transformType"].setValue("display" if display else "colorspace")
+
+    if config == "nuke-default":
+        node["colorspace"].setValue(default_space)
+    elif config.startswith("fn-nuke_"):
+        node["ocioDisplay"].setValue(display_space)
+    else:
+        node["colorspace"].setValue(aces_space)
 
 def gif_render():
     if not os.path.exists(FFMPEG):
@@ -51,14 +73,12 @@ def gif_render():
     write["file_type"].setValue(sequence_ext)
 
     # Set colorspace
+    isRec709 = False
     if nuke.activeViewer():
         viewer_process = nuke.activeViewer().node()["viewerProcess"].value()
-        if viewer_process == "Rec.709 (ACES)":
-            write["colorspace"].setValue("Output - Rec.709")
-        else:
-            write["colorspace"].setValue("Output - sRGB")
-    else:
-        write["colorspace"].setValue("Output - sRGB")
+        isRec709 = viewer_process.count("Rec.709") or viewer_process == "rec709"
+    colorspaces = ["rec709", "Output - Rec.709", "Rec.1886 Rec.709 - Display"] if isRec709 else ["sRGB", "Output - sRGB", "sRGB - Display"]
+    setColorspace(write, colorspaces)
 
     # Render
     try:
